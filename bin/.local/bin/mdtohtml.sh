b540d874ec1cb839e93dd0fb481243c5cec837e1
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2025-02-12 13:28:11 -0600

    mdtohtml with chapters

--- bin/.local/bin/mdtohtml.sh
+++ bin/.local/bin/mdtohtml.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-for file in $@; do
-    base_name=$(echo "$file" | cut -d '.' -f1)
-    pandoc -s -f markdown -t html5 -o "${base_name}.html" "$file" # -c "${base_name}.css"
+HTML_DIR="html"
+TOC_FILE="index.md"
+
+mkdir "$HTML_DIR"
+[[ $? -ne 0 ]] && exit 1
+
+echo "# Table of Contents" > "$TOC_FILE"
+
+for file in *.md; do
+    [ -e "$file" ] || continue
+
+    filename=$(basename -- "$file")
+    htmlname="${file%.md}.html"
+
+    lecture=$(echo "$filename" | cut -d'-' -f2- | sed 's/.md//')
+
+    if [ -z "$lecture" ]; then
+        lecture=$(head -n 1 "$file" | sed 's/# //')
+    fi
+
+    echo "- [$lecture]($htmlname)" >> "$TOC_FILE"
+
+    pandoc -s -f markdown -t html5 -o "${HTML_DIR}/$htmlname" "$file"
 done
+
+pandoc -s -f markdown -t html5 "$TOC_FILE" -o "${HTML_DIR}/${TOC_FILE%.md}.html"
+
