cf7934fb897d4c1013ed47f35bd948b38e9d6f40
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2025-10-24 22:25:21 -0500

    the sketchybar update

---
+++ sketchybar/.config/sketchybar/plugins/front_app_highlight.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# https://coolors.co/palette/231942-5e548e-9f86c0-be95c4-e0b1cb
+
+# Query current workspace from AeroSpace
+CURRENT_WORKSPACE=$(aerospace list-workspaces --focused | awk '{print $1}')
+
+# Reset all space icons' backgrounds
+for i in {1..10}; do
+  sketchybar --set space.$i background.color=0x40333333
+done
+
+# Highlight the current workspace icon
+if [[ -n "$CURRENT_WORKSPACE" ]]; then
+  sketchybar --set space.$CURRENT_WORKSPACE background.color=0xff5E548E
+fi
