f228e5005abb1df4295ed2a13de93ad925528389
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2025-11-01 16:30:51 -0500

    idek man

--- sketchybar/.config/sketchybar/plugins/aerospace_spaces.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace_spaces.sh
@@ -1,2 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
+# Display focused workspaces in sketchybar
+
+# if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+#     sketchybar --set $NAME background.drawing=on
+# else
+#     sketchybar --set $NAME background.drawing=off
+# fi
+
+SPACE_ID="$1"
+
+if [ "${FOCUSED_WORKSPACE:-}" = "$SPACE_ID" ]; then
+    sketchybar --set "space.$SPACE_ID" background.drawing=on
+else
+    sketchybar --set "space.$SPACE_ID" background.drawing=off
+fi
+
+# WORKSPACES=$(aerospace list-monitors | sed -E 's/^[0-9]+[[:space:]]+\|[[:space:]]+(.*)/\1/')
+#
+# get_displays() {
+#   while (($#)); do
+#     echo "$1"
+#     shift
+#   done
+# }
+#
+# get_displays "$WORKSPACES"
